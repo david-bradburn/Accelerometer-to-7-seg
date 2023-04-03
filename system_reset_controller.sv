0e0f5a8dc6d83babd4e4470c8d72aebf8c298f1a
David Bradburn <david.bradburn2@gmail.com>
2023-04-03 16:54:09 +0100

    Update system_reset_controller.sv
    Adding active high control to the reset controller

--- system_reset_controller.sv
+++ system_reset_controller.sv
@@ -1,7 +1,10 @@
-
+//
+// Holds the system in reset for N cycles configurable to be active high or low
+//
 
 module system_reset_controller #(
-    parameter NO_OF_CLK_CYCLES = 20
+    parameter NO_OF_CLK_CYCLES = 20,
+    parameter ACTIVE_HIGH = 1'b1
 )(
     input wire clk,
     output wire reset //positive reset
@@ -9,7 +12,7 @@ module system_reset_controller #(
 
     
     integer reset_count = 0;
-    assign reset = (reset_count == NO_OF_CLK_CYCLES) ? 0 : 1;
+    assign reset = (reset_count == NO_OF_CLK_CYCLES) ? ~ACTIVE_HIGH : ACTIVE_HIGH;
 
     always_ff @(posedge clk) begin
         if(reset_count < NO_OF_CLK_CYCLES) begin
