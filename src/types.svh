30b8058f98cce71605bca0471d429623a883d710
David Bradburn <david.bradburn2@gmail.com>
2023-04-04 11:55:27 +0100

    ifndef update
    Allows the types file to be included everywhere and not create errors :)
    Also cause we have ran the sim we have a lot more gitignores :)

--- src/types.svh
+++ src/types.svh
@@ -1,15 +1,18 @@
 //Header file for types declarations
-
-typedef enum { IDLE = 0, 
-               START = 1, 
-               SEND_DEVICE_ADDRESS = 3,
-               SEND_REGISTER_ADDRESS = 4,
-               REG_ADDRESS_ACK = 5,
-               SEND_WRITE_DATA = 6,
-               RESTART = 7,
-               SEND_DEVICE_ADDRESS_AGAIN = 8,
-               DEV_ADDRESS_ACK_AGAIN = 9,
-               RECEIVE_READ_DATA = 10,
-               SEND_NACK = 11,
-               STOP = 12,
-               ERROR = 255 } i2c_state_e; // might want to rename some of these state names cause they aren't great
+`ifndef I2C_STATE
+    typedef enum { IDLE = 0, 
+                START = 1, 
+                SEND_DEVICE_ADDRESS = 2,
+                DEV_ADDRESS_ACK = 3,
+                SEND_REGISTER_ADDRESS = 4,
+                REG_ADDRESS_ACK = 5,
+                SEND_WRITE_DATA = 6,
+                RESTART = 7,
+                SEND_DEVICE_ADDRESS_AGAIN = 8,
+                DEV_ADDRESS_ACK_AGAIN = 9,
+                RECEIVE_READ_DATA = 10,
+                SEND_NACK = 11,
+                STOP = 12,
+                ERROR = 255 } i2c_state_e; // might want to rename some of these state names cause they aren't great
+    `define I2C_STATE
+`endif
